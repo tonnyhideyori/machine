468d5e4324badc69553caf4a50f340c086942b03
edwin ismail <typhoon_ed@yahoo.com>
2018-11-28 19:08:53 +0100

    classification and regression
    this involved classififcation algorithmis and decision trees regression algorithm

---
+++ ML/Part3/Section 14 - Logistic Regression/logistic.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+dataset = pd.read_csv("Social_Network_Ads.csv")
+x = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=1/4, random_state=0)
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+classifier = LogisticRegression(random_state=0)
+classifier.fit(x_train, y_train)
+y_pred = classifier.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred)
+
+
